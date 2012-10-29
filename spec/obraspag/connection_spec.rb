9dd85bc4e8f3e693f9febb7b46227dc6b65558b7
Matheus Bodo <mcbodo@gmail.com>
2012-10-29 19:50:05 -0200

    Renaming folder

---
+++ spec/obraspag/connection_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Braspag::Connection do
+    context "#wsdl_url" do
+        it "should receive production url" do
+            Braspag::Connection.new.wsdl_url.should eq("production_url")
+        end
+
+        it "should receive homolog url" do
+            Braspag::Connection.new(:homolog).wsdl_url.should eq("https://homologacao.pagador.com.br/webservice/pagadorTransaction.asmx")
+        end
+    end
+end
