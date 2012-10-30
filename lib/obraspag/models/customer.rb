e368b4c086389f58e905914033d082f223c08ed7
Luis Daher <luisotaviodaher@gmail.com>
2012-10-30 17:44:40 -0200

    basic serialization for customer

--- lib/obraspag/models/customer.rb
+++ lib/obraspag/models/customer.rb
@@ -8,6 +8,34 @@ module Braspag
     validates :name, :presence => true
     validates :email, :presence => true
     
+    def to_hash
+      {
+        "CustomerIdentity" => "1234",
+        "CustomerName" => "Matheus",
+        "CustomerEmail" => "matheus.bodo@olook.com.br",
+        "CustomerAddressData" => {
+          "Street" => "Rua de Casa",
+          "Number" => "123",
+          "Complement" => "",
+          "District" => "Pinheiros",
+          "ZipCode" => "05425-070",
+          "City" => "Sao Paulo",
+          "State" => "SP",
+          "Country" => "Brasil"
+        },
+        "DeliveryAddressData" => {
+          "Street" => "Rua de Casa",
+          "Number" => "123",
+          "Complement" => "",
+          "District" => "Pinheiros",
+          "ZipCode" => "05425-070",
+          "City" => "Sao Paulo",
+          "State" => "SP",
+          "Country" => "Brasil"
+        }
+      }      
+    end
+
   end
 end
 
