7afa306664e93232d62d282e3443992449b6166b
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 20:23:00 -0200

    usign builder to generate hash

--- lib/authorize.rb
+++ lib/authorize.rb
@@ -1,67 +1,77 @@
 require 'savon'
+require './lib/obraspag'
 
-client = Savon::Client.new "https://homologacao.pagador.com.br/webservice/pagadorTransaction.asmx?WSDL"
+# client = Savon::Client.new "https://homologacao.pagador.com.br/webservice/pagadorTransaction.asmx?WSDL"
+# 
+# authorize_body = 
+#   {
+#     "request" => {
+#       "RequestId" => "00000000-0000-0000-0000-000000000007",
+#           "Version" =>"1.0",
+#       "OrderData" => {
+#         "MerchantId" => "540BA6EE-39D7-3DC1-D87D-7F82C49A3598",
+#         "OrderId" => "0006"
+#       },
+#       "CustomerData" => {
+#         "CustomerIdentity" => "1234",
+#         "CustomerName" => "Matheus",
+#         "CustomerEmail" => "matheus.bodo@olook.com.br",
+#         "CustomerAddressData" => {
+#           "Street" => "Rua de Casa",
+#           "Number" => "123",
+#           "Complement" => "",
+#           "District" => "Pinheiros",
+#           "ZipCode" => "05425-070",
+#           "City" => "Sao Paulo",
+#           "State" => "SP",
+#           "Country" => "Brasil"
+#         },
+#         "DeliveryAddressData" => {
+#           "Street" => "Rua de Casa",
+#           "Number" => "123",
+#           "Complement" => "",
+#           "District" => "Pinheiros",
+#           "ZipCode" => "05425-070",
+#           "City" => "Sao Paulo",
+#           "State" => "SP",
+#           "Country" => "Brasil"
+#         }
+#       },
+#       "PaymentDataCollection" => {
+#         "PaymentDataRequest" => { 
+#           "PaymentMethod" => "997",
+#           "Amount" => "500",
+#           "TransactionType" => "1",
+#           "Currency" => "BRL",
+#           "Country" => "BRA",
+#           "NumberOfPayments" => "1",
+#           "PaymentPlan" => "0",
+#           "TransactionType" => "1",
+#           "CardHolder" => "Comprador Teste",
+#           "CardNumber" => "0000000000000001",
+#           "CardSecurityCode" => "123",
+#           "CardExpirationDate" => "05/2018",
+#           "AdditionalDataCollection" => "",
+#           :attributes! => { "ins0:AdditionalDataCollection" => { "xsi:nil" => "true" }  }
+#         },
+#         :attributes! => { 
+#           "ins0:PaymentDataRequest" => { "xsi:type" => "ins0:CreditCardDataRequest" }
+#         }
+#       }
+#     }
+#   }
 
-authorize_body = 
-  {
-    "request" => {
-      "RequestId" => "00000000-0000-0000-0000-000000000007",
-          "Version" =>"1.0",
-      "OrderData" => {
-        "MerchantId" => "540BA6EE-39D7-3DC1-D87D-7F82C49A3598",
-        "OrderId" => "0006"
-      },
-      "CustomerData" => {
-        "CustomerIdentity" => "1234",
-        "CustomerName" => "Matheus",
-        "CustomerEmail" => "matheus.bodo@olook.com.br",
-        "CustomerAddressData" => {
-          "Street" => "Rua de Casa",
-          "Number" => "123",
-          "Complement" => "",
-          "District" => "Pinheiros",
-          "ZipCode" => "05425-070",
-          "City" => "Sao Paulo",
-          "State" => "SP",
-          "Country" => "Brasil"
-        },
-        "DeliveryAddressData" => {
-          "Street" => "Rua de Casa",
-          "Number" => "123",
-          "Complement" => "",
-          "District" => "Pinheiros",
-          "ZipCode" => "05425-070",
-          "City" => "Sao Paulo",
-          "State" => "SP",
-          "Country" => "Brasil"
-        }
-      },
-      "PaymentDataCollection" => {
-        "PaymentDataRequest" => { 
-          "PaymentMethod" => "997",
-          "Amount" => "500",
-          "TransactionType" => "1",
-          "Currency" => "BRL",
-          "Country" => "BRA",
-          "NumberOfPayments" => "1",
-          "PaymentPlan" => "0",
-          "TransactionType" => "1",
-          "CardHolder" => "Comprador Teste",
-          "CardNumber" => "0000000000000001",
-          "CardSecurityCode" => "123",
-          "CardExpirationDate" => "05/2018",
-          "AdditionalDataCollection" => "",
-          :attributes! => { "ins0:AdditionalDataCollection" => { "xsi:nil" => "true" }  }
-        },
-        :attributes! => { 
-          "ins0:PaymentDataRequest" => { "xsi:type" => "ins0:CreditCardDataRequest" }
-        }
-      }
-    }
-  }
+order = Braspag::OrderBuilder.new(0006)
 
-response = client.request :authorize_transaction do
-  soap.body = authorize_body
-end
+address = Braspag::AddressBuilder.new.with_street("Rua de Casa").with_number("123").with_complement("").with_district("Pinheiros")
+.with_zip_code("05425-070").with_city("Sao Paulo").with_state("SP").with_country("Brasil")
 
-puts response.to_hash
+customer = Braspag::CustomerBuilder.new.with_customer_id("1234").with_customer_name("Matheus").with_customer_email("matheus.bodo@olook.com.br")
+.with_customer_address(address).with_delivery_address(address)
+
+request = Braspag::AuthorizeTransactionRequestBuilder.new.with_request_id("00000000-0000-0000-0000-000000000007")
+.for_order(order).for_customer(customer).with_payment_request().build
+
+authorize_body = request.to_hash
+
+puts authorize_body
