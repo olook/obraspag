de9778f81e225c4654faf39fc4717519b4343f7c
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 16:15:47 -0200

    improving authorize transaction request builder

--- lib/obraspag/models/customer.rb
+++ lib/obraspag/models/customer.rb
@@ -1,6 +1,13 @@
 module Braspag
   class Customer
+    include ::ActiveAttr::Model
+
     attr_accessor :id, :name, :email
+
+    validates :id, :presence => true
+    validates :name, :presence => true
+    validates :email, :presence => true
+    
   end
 end
 
