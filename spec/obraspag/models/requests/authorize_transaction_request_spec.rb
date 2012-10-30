313bf31a33b01e5162a2c799f17494d44e102278
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-30 14:19:04 -0200

    Organize spec

--- spec/obraspag/models/requests/authorize_transaction_request_spec.rb
+++ spec/obraspag/models/requests/authorize_transaction_request_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Braspag::AuthorizeTransactionRequest do
 
-  it "should create with correct params" do
-      subject.build_request_object.should be_valid
-  end
+    it "should create with correct params" do
+        request_object = Braspag::AuthorizeTransactionRequest.build_request_object
+        request_object.should be_an_instance_of(Braspag::AuthorizeTransactionRequest)
+    end
 
 end
