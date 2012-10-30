9c9d4d5cefb6578398d1d572ec1a8cce6867d208
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-30 10:21:14 -0200

    Add initial test on object

--- spec/models/authorize_transaction_request_spec.rb
+++ spec/models/authorize_transaction_request_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Braspag::AuthorizeTransactionRequest do
 
     it "should create with correct params" do
+        subject.build_request_object.should be_valid
     end
 
 end
