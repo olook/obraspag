bf16b840f4e54369946230bf5aa262759bad91b3
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 16:32:54 -0200

    testing authorize_transaction_request_builder

--- spec/obraspag/builders/authorize_transaction_request_builder_spec.rb
+++ spec/obraspag/builders/authorize_transaction_request_builder_spec.rb
@@ -4,18 +4,45 @@ describe Braspag::AuthorizeTransactionRequestBuilder do
 
   context "with valid build data" do
 
-    builder = Braspag::AuthorizeTransactionRequestBuilder.new
-    builder.with_request_id('1234')
+    subject { complete_builder }
+
+    it "should build an AuthorizeTransactionRequest object" do
+      subject.build.should_not be_nil
+    end
+
+  end
+
+  context "with invalid build data" do
+    subject { complete_builder }
+
+    it "should raise exception when AuthorizeTransactionRequest is not valid" do
+      subject.with_request_id(nil)
+      expect { subject.build }.to raise_error
+    end
+
+    it "should raise exception when Order is not valid" do
+      subject.with_order_number(nil)
+      expect { subject.build }.to raise_error
+    end
+
+    it "should raise exception when Customer is not valid" do
+      subject.with_customer_name(nil)
+      expect { subject.build }.to raise_error
+    end
+
+  end
+
+  private
+
+  def complete_builder
+      builder = Braspag::AuthorizeTransactionRequestBuilder.new
+      builder.with_request_id('1234')
       .with_order_number(1234)
       .with_payment_method(Braspag::PAYMENT_METHOD[:braspag])
       .with_customer_id(1)
       .with_customer_name('Teste')
       .with_customer_email('teste@olook.com.br')
-
-    it "should build an AuthorizeTransactionRequest object" do
-      builder.build.should_not be_nil
+      builder
     end
-
-  end
 
 end
