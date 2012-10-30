a7ab089a409b9851a7cdecfdd4641627840bf41b
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 14:14:39 -0200

    implementing webservice methods

--- lib/obraspag/webservice.rb
+++ lib/obraspag/webservice.rb
@@ -5,16 +5,31 @@ module Braspag
     		@connection = Braspag::Connection.new(env)
     	end
 
-    	def authorize(authorize_transaction_request)
+    	def authorize_transaction(authorize_transaction_request)
+    		call_webservice(:authorize_transaction, authorize_transaction_request)
     	end
 
-    	def capture(capture_transaction_request)
+    	def capture_credit_card_transaction(capture_credit_card_transaction_request)
+    		call_webservice(:capture_credit_card_transaction, capture_credit_card_transaction_request)
     	end
 
-    	def void(void_transaction_request)
+    	def void_credit_card_transaction(void_credit_card_transaction_request)
+    		call_webservice(:void_credit_card_transaction, void_credit_card_transaction_request)
     	end
 
-    	def refund(refund_transaction_request)
+    	def refund_credit_card_transaction(refund_credit_card_transaction_request)
+    		call_webservice(:refund_credit_card_transaction, refund_credit_card_transaction_request)
+    	end
+
+    	private
+
+    	def call_webservice(method, request)
+    		if request.valid?
+	    		body = request.to_hash
+	    		@connection.call_webservice(method, body)
+	    	else
+	    		#TODO Raise exception
+	    	end
     	end
     end
 end
