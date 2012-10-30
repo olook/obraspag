44e70317999660bdd831a7f48bfd7b569b229db1
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 15:12:09 -0200

    adding validation to order

--- lib/obraspag/connection.rb
+++ lib/obraspag/connection.rb
@@ -2,7 +2,6 @@ module Braspag
     class Connection
         HOMOLOG_URL = "https://homologacao.pagador.com.br/webservice/pagadorTransaction.asmx"
         PRODUCTION_URL = "production_url"
-        attr_accessor :merchantId
 
         def initialize(env = :production)
             @env = env
