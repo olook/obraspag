195d574a59953f1583120fc6f6a087e1900fba94
Luis Daher <luisotaviodaher@gmail.com>
2012-10-30 17:10:29 -0200

    attributes added to authorize transaction request

--- lib/obraspag/models/requests/authorize_transaction_request.rb
+++ lib/obraspag/models/requests/authorize_transaction_request.rb
@@ -1,21 +1,27 @@
 module Braspag
 
   class AuthorizeTransactionRequest
-
     include ::ActiveAttr::Model
-    attribute :version 
+
+    attribute :request_id
+    attribute :version
     attribute :order_data
     attribute :payment_data_collection
     attribute :customer_data
-    attribute :request_id
+
+    attr_accessor :request_id, :version, :order_data, :payment_data_collection, :customer_data
+
     validates :request_id, {:presence => true}
 
     def initialize
-      self.order_data = Order.new
-      self.payment_data_collection = []
-      self.customer_data = []
-      self.version = CONTRACT_VERSION
+      @order_data = Order.new
+      @payment_data_collection = []
+      @customer_data = []
+      @version = CONTRACT_VERSION
     end
 
+    def to_hash
+      "TESTE"
+    end
   end
 end
