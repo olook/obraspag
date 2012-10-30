d501ced2537f5ad8e561bf7e654d7aa8d38839af
Luis Daher <luisotaviodaher@gmail.com>
2012-10-30 17:30:35 -0200

    authorize transaction request and order to_hash methods partially implemented

--- lib/obraspag/models/order.rb
+++ lib/obraspag/models/order.rb
@@ -2,14 +2,28 @@ module Braspag
   class Order
     include ::ActiveAttr::Model
 
-    attr_accessor :order_number, :payment_method, :customer
+    attribute :order_number
+    attribute :payment_method
+    attribute :customer
+    attribute :merchant_id
 
-    validates :order_number, :presence => true
+    attr_accessor :number, :payment_method, :customer, :merchant_id
+
+    validates :number, :presence => true
     validates :payment_method, :presence => true
     validates :customer, :presence => true
 
     def initialize
       @customer = Customer.new
+      @merchant_id = "540BA6EE-39D7-3DC1-D87D-7F82C49A3598"
     end
+
+    def to_hash
+      {
+        "MerchantId" => self.merchant_id,
+        "OrderId" => self.number
+      }
+    end
+
   end
 end
--- lib/obraspag/models/requests/authorize_transaction_request.rb
+++ lib/obraspag/models/requests/authorize_transaction_request.rb
@@ -21,7 +21,59 @@ module Braspag
     end
 
     def to_hash
-      self.attributes
+      {
+        "request" => {
+          "RequestId" => self.request_id,
+          "Version" =>self.version,
+          "OrderData" => self.order_data.to_hash,
+          "CustomerData" => {
+            "CustomerIdentity" => "1234",
+                  "CustomerName" => "Matheus",
+                  "CustomerEmail" => "matheus.bodo@olook.com.br",
+                  "CustomerAddressData" => {
+                    "Street" => "Rua de Casa",
+                    "Number" => "123",
+                    "Complement" => "",
+                    "District" => "Pinheiros",
+                    "ZipCode" => "05425-070",
+                    "City" => "Sao Paulo",
+                    "State" => "SP",
+                    "Country" => "Brasil"
+                  },
+                  "DeliveryAddressData" => {
+                    "Street" => "Rua de Casa",
+                    "Number" => "123",
+                    "Complement" => "",
+                    "District" => "Pinheiros",
+                    "ZipCode" => "05425-070",
+                    "City" => "Sao Paulo",
+                    "State" => "SP",
+                    "Country" => "Brasil"
+                  }
+          },
+          "PaymentDataCollection" => {
+            "PaymentDataRequest" => { 
+              "PaymentMethod" => "997",
+              "Amount" => "500",
+                    "TransactionType" => "1",
+                    "Currency" => "BRL",
+                    "Country" => "BRA",
+                    "NumberOfPayments" => "1",
+                    "PaymentPlan" => "0",
+                    "TransactionType" => "1",
+                    "CardHolder" => "Comprador Teste",
+                    "CardNumber" => "0000000000000001",
+                    "CardSecurityCode" => "123",
+                    "CardExpirationDate" => "05/2018",
+                    "AdditionalDataCollection" => "",
+                    :attributes! => { "ins0:AdditionalDataCollection" => { "xsi:nil" => "true" }  }
+                  },
+                  :attributes! => { 
+                    "ins0:PaymentDataRequest" => { "xsi:type" => "ins0:CreditCardDataRequest" }
+                  }
+          }
+        }
+      }
     end
   end
 end
