c3de88e91464edf2b80472f27bc2dbb6a0253573
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-30 10:59:14 -0200

    loading files

--- lib/obraspag.rb
+++ lib/obraspag.rb
@@ -1,3 +1,4 @@
 require 'obraspag/connection'
-Dir["./lib/models/*.rb"].each {|file| require file }
-Dir["./lib/models/requests/*.rb"].each {|file| require file }
+Dir["./lib/obraspag/models/*.rb"].each {|file| require file }
+Dir["./lib/obraspag/models/requests/*.rb"].each {|file| require file }
+Dir["./lib/obraspag/models/payments/*.rb"].each {|file| require file }
--- spec/obraspag/models/payments/credit_card_spec.rb
+++ spec/obraspag/models/payments/credit_card_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Braspag::CreditCard do
-    subject = CreditCard.new
+    subject = Braspag::CreditCard.new
     context "#validate_date" do
         subject.year = 2012
         subject.month = 01
