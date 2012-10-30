62aea9c7067dbed2e499892c84c2464a366bf5e8
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-30 10:28:56 -0200

    removing old models credit card

--- lib/models/payments/credit_card.rb
+++ lib/models/payments/credit_card.rb
@@ -2,19 +2,17 @@ module Braspag
     class CreditCard
         attr_accessor :holder_name, :number, :month, :year, :verification_value, :id, :currency, :security_code, :number_of_payments
 
-        class ExpiratorValidator < ActiveModel::EachValidator
-            def validate_each(record, attribute, value)
-                begin
-                    year = record.year.try(:to_i)
-                    year = "20#{year}".to_i if year && year.to_s.size == 2
-
-                    month = record.month.try(:to_i)
-
-                    Date.new(year, month) if year && month
-                rescue ArgumentError
-                    record.errors.add attribute, "invalid date"
-                end
+        private
+        def validate_date
+            begin
+                year = self.year.try(:to_i)
+                year = "20#{year}".to_i if year && year.to_s.size == 2
+                month = record.month.try(:to_i)
+                Date.new(year, month) if year && month
+            rescue ArgumentError
+                self.errors.add attribute, "invalid date"
             end
         end
+
     end
 end
