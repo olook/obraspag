da2a9b24e32d26558fea48940d50987321064ed8
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 17:39:07 -0200

    adding address builder

---
+++ lib/obraspag/builders/address_builder.rb
@@ -0,0 +1,59 @@
+module Braspag
+  class AddressBuilder
+
+    def initialize
+      @address = Braspag::Address.new
+    end
+
+    def build
+      raise 'Address data is invalid.' unless @address.valid?
+      @address
+    end
+
+    def with_street(street)
+      @address.street = street
+      self
+    end
+
+    def with_number(number)
+      @address.number = number
+      self
+    end
+
+    def with_complement(complement)
+      @address.complement = complement
+      self
+    end
+
+    def with_district(district)
+      @address.district = district
+      self
+    end
+
+    def with_zip_code(zip_code)
+      @address.zip_code = zip_code
+      self
+    end
+
+    def with_city(city)
+      @address.city = city
+      self
+    end
+
+    def with_state(state)
+      @address.state = state
+      self
+    end
+
+    def with_country(country)
+      @address.country = country
+      self
+    end
+
+    def with_address_type(address_type)
+      @addres.address_type = address_type
+      self
+    end
+
+  end
+end
