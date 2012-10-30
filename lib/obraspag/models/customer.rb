26da36a82471b513865c694f1fa9809ac54e7754
Matheus Bodo <mcbodo@gmail.com>
2012-10-30 17:31:28 -0200

    adding address to customer

--- lib/obraspag/models/customer.rb
+++ lib/obraspag/models/customer.rb
@@ -2,11 +2,17 @@ module Braspag
   class Customer
     include ::ActiveAttr::Model
 
-    attr_accessor :id, :name, :email
+    attribute :id
+    attribute :name
+    attribute :email
+    attribute :customer_address
+    attribute :delivery_address
 
     validates :id, :presence => true
     validates :name, :presence => true
     validates :email, :presence => true
+    validates :customer_address, :presence => true
+    validates :delivery_address, :presence => true
     
   end
 end
