3359326fbd470924194dd3d0df08c82736f908f5
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-30 15:33:25 -0200

    Add validations on AuthorizeTransactionRequest class

--- lib/obraspag/models/requests/authorize_transaction_request.rb
+++ lib/obraspag/models/requests/authorize_transaction_request.rb
@@ -1,11 +1,16 @@
 module Braspag
+
   class AuthorizeTransactionRequest
+    include ::ActiveAttr::Model
     attr_accessor :request_id, :version, :order_data, :payment_data_collection, :customer_data
+
+    validates :request_id, {:presence => true}
 
     def initialize
       @order_data = []
       @payment_data_collection = []
       @customer_data = []
+      @version = "VERSAO IRA AQUI"
     end
 
   end
