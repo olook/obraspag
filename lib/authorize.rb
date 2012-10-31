1d6cba7c04e264617126d34bc5f287873b6f8a33
Matheus Augusto Bodo <mcbodo@gmail.com>
2012-10-30 23:17:41 -0200

    changing authorize script to use models to create the hash

--- lib/authorize.rb
+++ lib/authorize.rb
@@ -69,8 +69,13 @@ address = Braspag::AddressBuilder.new.with_street("Rua de Casa").with_number("12
 customer = Braspag::CustomerBuilder.new.with_customer_id("1234").with_customer_name("Matheus").with_customer_email("matheus.bodo@olook.com.br")
 .with_customer_address(address).with_delivery_address(address).build
 
+payment_request = Braspag::CreditCardBuilder.new.with_payment_method(Braspag::PAYMENT_METHOD[:braspag])
+.with_amount("500").with_transaction_type("1").with_currency("BRL").with_country("BRA").with_number_of_payments(1)
+.with_payment_plan("0").with_transaction_type("1").with_holder_name("Comprador Teste").with_card_number("0000000000000001")
+.with_security_code("123").with_expiration_month(05).with_expiration_year(2018).build
+
 request = Braspag::AuthorizeTransactionRequestBuilder.new.with_request_id("00000000-0000-0000-0000-000000000007")
-.for_order(order).for_customer(customer).build
+.for_order(order).for_customer(customer).with_payment_request(payment_request).build
 
 authorize_body = request.to_hash
 
