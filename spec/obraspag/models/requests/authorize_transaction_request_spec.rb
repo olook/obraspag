ac4a1692e151155166ea02bd35dde2fdaad6ef11
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-31 10:55:11 -0200

    Use factory request object for tests

--- spec/obraspag/models/requests/authorize_transaction_request_spec.rb
+++ spec/obraspag/models/requests/authorize_transaction_request_spec.rb
@@ -12,8 +12,12 @@ describe Braspag::AuthorizeTransactionRequest do
     end
 
     it "should be invalid without attributes" do
-      request_object.request_id = "1234"
-      request_object.valid?.should be_true
+      request_object_full.valid?.should be_true
+    end
+
+    it "should create a hash" do
+       pending
+       request_object_full.to_hash.should be_true
     end
 
 end
