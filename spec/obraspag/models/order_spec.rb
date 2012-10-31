4ff29289baa37b563b3be79c3019e8b731420bff
Luis Daher <luisotaviodaher@gmail.com>
2012-10-31 15:01:30 -0200

    order and customer specs ok

--- spec/obraspag/models/order_spec.rb
+++ spec/obraspag/models/order_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Braspag::Order do
 
-	context "order validation" do
+  subject { s =  Braspag::Order.new(1234) }
 
-    subject { Braspag::Order.new(1234) }
+	context "order validation" do
 
       it "should validate a valid order" do
         subject.valid?.should eq(true)
@@ -16,5 +16,12 @@ describe Braspag::Order do
 	    end
 
 	end
+
+  context "generated hash validation" do
+    let(:expected_hash) { {"MerchantId"=>"540BA6EE-39D7-3DC1-D87D-7F82C49A3598", "OrderId"=>1234} }
+    it "should generate the expected hash" do
+      subject.to_hash.should eq(expected_hash)
+    end
+  end
 
 end
