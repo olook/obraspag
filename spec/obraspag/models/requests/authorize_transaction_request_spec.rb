4c9b768e1508c26cbd03d1bce9f07a0ee1dd3ab7
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-31 10:54:29 -0200

    Add request_object factory with attributes

--- spec/obraspag/models/requests/authorize_transaction_request_spec.rb
+++ spec/obraspag/models/requests/authorize_transaction_request_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Braspag::AuthorizeTransactionRequest do
     let(:request_object) {Braspag::AuthorizeTransactionRequest.new}
+    let(:request_object_full) {
+        request_object.request_id = "1234"
+        request_object
+    }
+
     it "should be invalid without attributes" do
       request_object.valid?.should be_false
     end
