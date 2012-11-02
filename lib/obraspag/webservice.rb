66d73c3f9b49f6243c3feff5e40cc2f635c8393d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2012-11-02 14:27:16 -0200

    Indent file

--- lib/obraspag/webservice.rb
+++ lib/obraspag/webservice.rb
@@ -1,24 +1,25 @@
 module Braspag
+
   class Webservice
 
     def initialize(env)
-            @connection = Braspag::Connection.new(env)
+      @connection = Braspag::Connection.new(env)
     end
 
     def authorize_transaction(authorize_transaction_request)
-            call_webservice(:authorize_transaction, authorize_transaction_request)
+      call_webservice(:authorize_transaction, authorize_transaction_request)
     end
 
     def capture_credit_card_transaction(capture_credit_card_transaction_request)
-            call_webservice(:capture_credit_card_transaction, capture_credit_card_transaction_request)
+      call_webservice(:capture_credit_card_transaction, capture_credit_card_transaction_request)
     end
 
     def void_credit_card_transaction(void_credit_card_transaction_request)
-            call_webservice(:void_credit_card_transaction, void_credit_card_transaction_request)
+      call_webservice(:void_credit_card_transaction, void_credit_card_transaction_request)
     end
 
     def refund_credit_card_transaction(refund_credit_card_transaction_request)
-            call_webservice(:refund_credit_card_transaction, refund_credit_card_transaction_request)
+      call_webservice(:refund_credit_card_transaction, refund_credit_card_transaction_request)
     end
 
     def checkout(authorize_transaction_request)
