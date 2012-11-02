99ed04e0c32cf5f61e44c5c10a5e9963f9a483b4
Tiago Almeida <tig.luiz.almeida@gmail.com>
2012-11-02 14:28:26 -0200

    Add builder for cancel transaction method

---
+++ lib/obraspag/builders/void_credit_card_transaction_request_builder.rb
@@ -0,0 +1,25 @@
+module Braspag
+
+  class VoidCreditCardTransactionRequestBuilder
+
+    def initialize
+      @void_credit_card_transaction_request = VoidCreditCardTransactionRequest.new
+    end
+
+    def build
+     raise 'Void Credit Card Transaction Request is invalid.' unless @void_credit_card_transaction_request.valid?
+     raise 'Void Credit Card Transaction Request is invalid.' unless @void_credit_card_transaction_request.transaction_data_collection.valid?
+     @void_credit_card_transaction_request
+    end
+
+    def with_request_id(request_id)
+      @void_credit_card_transaction_request.request_id = request_id
+      self
+    end
+
+    def transaction_data_collection(transactions_data)
+      @void_credit_card_transaction_request.transaction_data_collection << transactions_data
+      self
+    end
+  end
+end
