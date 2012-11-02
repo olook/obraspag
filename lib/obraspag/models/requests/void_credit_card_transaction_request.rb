8e65cc61048e7f9c5783d3409dfb6f9ab89bca61
Tiago Almeida <tig.luiz.almeida@gmail.com>
2012-11-02 14:25:18 -0200

    Add VoidCreditCardTransaction class on braspag mudule end add initialize

--- lib/obraspag/models/requests/void_credit_card_transaction_request.rb
+++ lib/obraspag/models/requests/void_credit_card_transaction_request.rb
@@ -1,13 +1,21 @@
-class VoidCreditCardTransactionRequest
+module Braspag
+  class VoidCreditCardTransactionRequest
 
-  include ::ActiveAttr::Model
+    include ::ActiveAttr::Model
 
-  attribute :request_id
-  attribute :version
-  attribute :merchant_id
-  attribute :transaction_data_collection
-  attr_accessor :request_id, :version, :merchant_id, :transaction_data_collection
+    attribute :request_id
+    attribute :version
+    attribute :merchant_id
+    attribute :transaction_data_collection
+    attr_accessor :request_id, :version, :merchant_id, :transaction_data_collection
 
-  validates :request_id, :version, :merchant_id, :transaction_data_collection , {:presence => true}
+    validates :request_id, :version, :merchant_id, {:presence => true}
 
+    def initialize
+      @merchant_id = MERCHANT_ID
+      @version = CONTRACT_VERSION
+    end
+
+
+  end
 end
