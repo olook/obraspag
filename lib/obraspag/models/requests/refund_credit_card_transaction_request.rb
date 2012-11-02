4a3cfac64331686b8fde7759d9cccf59a607c7de
Tiago Almeida <tig.luiz.almeida@gmail.com>
2012-11-02 14:23:31 -0200

    Add RefundCreditCardTransactionRequest on braspag module

--- lib/obraspag/models/requests/refund_credit_card_transaction_request.rb
+++ lib/obraspag/models/requests/refund_credit_card_transaction_request.rb
@@ -1,13 +1,17 @@
-class RefundCreditCardTransactionRequest
+module Braspag
 
-  include ::ActiveAttr::Model
+  class RefundCreditCardTransactionRequest
 
-  attribute :request_id
-  attribute :version
-  attribute :merchant_id
-  attribute :transaction_data_collection
-  attr_accessor :request_id, :version, :merchant_id, :transaction_data_collection
+    include ::ActiveAttr::Model
 
-  validates :request_id, :version, :merchant_id, :transaction_data_collection , {:presence => true}
+    attribute :request_id
+    attribute :version
+    attribute :merchant_id
+    attribute :transaction_data_collection
 
+    attr_accessor :request_id, :version, :merchant_id, :transaction_data_collection
+
+    validates :request_id, :version, :merchant_id, :transaction_data_collection , {:presence => true}
+
+  end
 end
