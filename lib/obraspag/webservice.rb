bd788a18c6fec8d74bd35059751bbcab7ef3ff81
Tiago Almeida <tig.luiz.almeida@gmail.com>
2012-11-02 10:58:33 -0200

    Indent code

--- lib/obraspag/webservice.rb
+++ lib/obraspag/webservice.rb
@@ -1,25 +1,25 @@
 module Braspag
   class Webservice
 
-  	def initialize(env)
-  		@connection = Braspag::Connection.new(env)
-  	end
+    def initialize(env)
+            @connection = Braspag::Connection.new(env)
+    end
 
-  	def authorize_transaction(authorize_transaction_request)
-  		call_webservice(:authorize_transaction, authorize_transaction_request)
-  	end
+    def authorize_transaction(authorize_transaction_request)
+            call_webservice(:authorize_transaction, authorize_transaction_request)
+    end
 
-  	def capture_credit_card_transaction(capture_credit_card_transaction_request)
-  		call_webservice(:capture_credit_card_transaction, capture_credit_card_transaction_request)
-  	end
+    def capture_credit_card_transaction(capture_credit_card_transaction_request)
+            call_webservice(:capture_credit_card_transaction, capture_credit_card_transaction_request)
+    end
 
-  	def void_credit_card_transaction(void_credit_card_transaction_request)
-  		call_webservice(:void_credit_card_transaction, void_credit_card_transaction_request)
-  	end
+    def void_credit_card_transaction(void_credit_card_transaction_request)
+            call_webservice(:void_credit_card_transaction, void_credit_card_transaction_request)
+    end
 
-  	def refund_credit_card_transaction(refund_credit_card_transaction_request)
-  		call_webservice(:refund_credit_card_transaction, refund_credit_card_transaction_request)
-  	end
+    def refund_credit_card_transaction(refund_credit_card_transaction_request)
+            call_webservice(:refund_credit_card_transaction, refund_credit_card_transaction_request)
+    end
 
     def checkout(authorize_transaction_request)
         authorize_response = authorize_transaction(authorize_transaction_request)
@@ -29,13 +29,13 @@ module Braspag
     private
 
     def checkout_response(authorize_response)
-        if response_success?(authorize_response)
-            capture_request = create_capture_credit_card_request(authorize_response)
-            capture_response = capture_credit_card_transaction(capture_request)
-            [authorize_response, capture_response]
-        else
-            authorize_response
-        end
+      if response_success?(authorize_response)
+          capture_request = create_capture_credit_card_request(authorize_response)
+          capture_response = capture_credit_card_transaction(capture_request)
+          [authorize_response, capture_response]
+      else
+          authorize_response
+      end
     end
 
     def response_success?(authorize_response)
@@ -50,9 +50,9 @@ module Braspag
       Braspag::CaptureCreditCardTransactionRequestBuilder.new.with_request_id(request_id).with_transaction_request(transaction_request).build
     end
 
-  	def call_webservice(method, request)
-  		body = request.to_hash
-  		@connection.call_webservice(method, body)
-  	end
+    def call_webservice(method, request)
+      body = request.to_hash
+      @connection.call_webservice(method, body)
+    end
   end
 end
