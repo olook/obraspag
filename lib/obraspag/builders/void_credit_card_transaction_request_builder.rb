4a987299d0359160f8367877bd40d624ea9dc179
Tiago Almeida <tiago.almeida@olook.com.br>
2012-11-05 09:49:28 -0200

    Add new raise message to transaction data collect

--- lib/obraspag/builders/void_credit_card_transaction_request_builder.rb
+++ lib/obraspag/builders/void_credit_card_transaction_request_builder.rb
@@ -9,6 +9,7 @@ module Braspag
     def build
      raise 'Void Credit Card Transaction Request is invalid.' unless @void_credit_card_transaction_request.valid?
      raise 'Void Credit Card Transaction Request is invalid.' unless @void_credit_card_transaction_request.transaction_data_collection.valid?
+     raise 'Void Credit Card Transaction Data Collection is invalid.' unless @void_credit_card_transaction_request.transaction_data_collection.valid?
      @void_credit_card_transaction_request
     end
 
