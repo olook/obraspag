8fc6b8671884fc4e035ac073e8dfa878bb5bbff7
Tiago Almeida <tiago.almeida@olook.com.br>
2012-11-05 16:09:37 -0200

    Refactor code to transform refund, capture and void to same object

---
+++ lib/obraspag/builders/credit_card_transaction_request_builder.rb
@@ -0,0 +1,24 @@
+module Braspag
+  class CreditCardTransactionRequestBuilder
+
+    def initialize
+      @credit_card_transaction_request = Braspag::CreditCardTransactionRequest.new
+    end
+
+    def build
+      raise 'Credit Card Transaction Request is invalid.' unless @credit_card_transaction_request.valid?
+      raise 'Credit Card Transaction Data Collection is empty.' if @credit_card_transaction_request.transaction_data_collection.empty?
+      @credit_card_transaction_request
+    end
+
+    def with_request_id(request_id)
+      @credit_card_transaction_request.request_id = request_id
+      self
+    end
+
+    def with_transaction_request(transaction_request)
+      @credit_card_transaction_request.transaction_data_collection << transaction_request.to_hash
+      self
+    end
+  end
+end
