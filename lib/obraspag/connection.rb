f58f8c7c9e3bd91dc66e895a4d26d5422d9d1032
Matheus Bodo <mcbodo@gmail.com>
2012-12-10 12:06:10 -0200

    removes sensitive data from log

--- lib/obraspag/connection.rb
+++ lib/obraspag/connection.rb
@@ -8,7 +8,7 @@ module Braspag
     end
 
     def call_webservice(method, body)
-      logger().info("Calling method #{method.try(:to_s)} with body: #{body.try(:to_s)}")
+      logger().info("Calling method #{method.try(:to_s)} with body: #{remove_sensitive_data(body.dup)}")
       client = ::Savon::Client.new wsdl_url
       client.http.read_timeout = 10000
       response = client.request :wsdl, method do
@@ -24,6 +24,23 @@ module Braspag
 
     def production?
       @env == :production
+    end
+
+    def remove_sensitive_data(params)
+      case params.class.to_s
+        when "Hash"
+          Hash[params.map { |k,v| 
+            if k == :card_number
+              v = "XXXXXXXXXXXXXXXX"
+            elsif k == :security_code
+              v = "XXX"
+            end
+            [k, remove_sensitive_data(v)] } ]
+        when "Array"
+          params.map { |v| remove_sensitive_data(v) }
+        else
+          params
+      end
     end
 
     def logger
