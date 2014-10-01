56b131fe43b2760b6f342880b036e56ae357050e
Luis Daher <luisotaviodaher@gmail.com>
2014-09-30 21:11:57 -0300

    gyoku as a filter

--- lib/obraspag/models/customer.rb
+++ lib/obraspag/models/customer.rb
@@ -15,13 +15,13 @@ module Braspag
     validates :email, :presence => true
     
     def to_hash
-      Gyoku.xml({
+      {
         :customer_identity => self.id.to_s,
         :customer_name => self.name,
         :customer_email => self.email,
         :customer_address_data => self.customer_address.to_hash,
         :delivery_address_data => self.delivery_address.to_hash
-      }, { :key_converter => :camelcase })    
+      }    
     end
 
   end
